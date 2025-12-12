bf35d302c28ac88aa74fa2aef8e721f52d0a70af
Romi Setiawan <romi36825@gmail.com>
2025-12-12 11:41:57 +0700

    Fixs Bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,8 @@ use App\Http\Middleware\RoleMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        // PERUBAHAN: Baris ini wajib ada agar file routes/api.php terbaca!
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
